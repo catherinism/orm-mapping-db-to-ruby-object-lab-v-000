104c51124947e135598c14e50ac3960cf7ca3338
Catherine Salazar <catcreates13@gmail.com>
2018-09-06 01:28:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
                 COUNT(grade)
               SQL
 
-              DB[:conn].execute(sql, grade).map do |row|
+              DB[:conn].execute(sql).map do |row|
                 self.new_from_db(row)
               end
        end
