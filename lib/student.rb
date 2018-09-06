f784dbc7e6eec90b60c468d19ee04f0195c81c38
Catherine Salazar <catcreates13@gmail.com>
2018-09-06 01:08:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,16 +11,16 @@ class Student
     new_student
   end
 
-  def self.all
-    sql = <<-SQL
-        SELECT *
-        FROM students
-      SQL
-
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-    end
-  end
+  # def self.all
+  #   sql = <<-SQL
+  #       SELECT *
+  #       FROM students
+  #     SQL
+  #
+  #     DB[:conn].execute(sql).map do |row|
+  #       self.new_from_db(row)
+  #   end
+  # end
 
   def self.find_by_name(name)
     # find the student in the database given a name
