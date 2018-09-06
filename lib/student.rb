c81ff71dcbed8f86b0d45c1d4da8d1290e7382ff
Catherine Salazar <catcreates13@gmail.com>
2018-09-06 01:11:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,16 @@ class Student
          end.first
        end
 
-       def self.all_students_in_grade_9(name)
+       def self.all_students_in_grade_9
+         sql = <<-SQL
+                SELECT *
+                FROM students
+                WHERE grade = 9
+              SQL
 
+              DB[:conn].execute(sql, name).map do |row|
+                self.new_from_db(row)
+              end.first
        end
 
   def save
